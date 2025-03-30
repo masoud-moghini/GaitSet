ea494b38c22577e57c95a0b458ca4d63f7c49a77
masoud-moghini <koosha_pooyeshgar@outlook.com>
2025-03-30 20:08:52 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,22 @@
 conf = {
-    "WORK_PATH": "./work",
+    "WORK_PATH": "/content/GaitSet/work",
     "CUDA_VISIBLE_DEVICES": "0,1,2,3",
     "data": {
-        'dataset_path': "your_dataset_path",
+        'dataset_path': "/content/sample",
         'resolution': '64',
         'dataset': 'CASIA-B',
         # In CASIA-B, data of subject #5 is incomplete.
         # Thus, we ignore it in training.
         # For more detail, please refer to
         # function: utils.data_loader.load_data
-        'pid_num': 73,
+        'pid_num': 5,
         'pid_shuffle': False,
     },
     "model": {
         'hidden_dim': 256,
         'lr': 1e-4,
         'hard_or_full_trip': 'full',
-        'batch_size': (8, 16),
+        'batch_size': (4, 8),
         'restore_iter': 0,
         'total_iter': 80000,
         'margin': 0.2,
