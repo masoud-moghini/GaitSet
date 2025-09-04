75de7dec07f78407ceaea86f32e52aa0dde81aba
masoud-moghini <koosha_pooyeshgar@outlook.com>
2025-09-04 15:16:03 +0330

    print logs

--- model/utils/evaluator.py
+++ model/utils/evaluator.py
@@ -64,7 +64,8 @@ def evaluation(data, config, probe_idx=None, top_k=1):
                         print(f'view:{view}')
                         print(f'p is :{p}\nseq_type:{seq_type}\ngallary_seq:{gallery_seq}\tprobe_seq:{probe_seq}')
                         print(f'probe_view:{probe_view}\ngallary_view:{gallery_view}')
-                        print(f'probe_x:{probe_x}\nprobe_y:{probe_y}')
+                        print(f'probe_x shape:{probe_x.shape}\nprobe_y:{probe_y}')
+                        print(f'gallery_x shape:{gallery_x.shape}\ngallery_y:{gallery_y}')
                         print(f'num_rank:{num_rank}')
                         # print(f'probe_path:{probe_path}\ngallary_path:{gallery_path}')
                         # Compute distances on GPU
